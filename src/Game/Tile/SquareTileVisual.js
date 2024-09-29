730c9e65ae9f5b38bc2a87666687e45e5caec96d
Konnor <wordofessence@gmail.com>
2024-09-29 10:05:18 -0600

    Beginning of visual element of a game tile.

---
+++ src/Game/Tile/SquareTileVisual.js
@@ -0,0 +1,5 @@
+import styles from './Card.module.css'
+
+function SquareTileVisual(posx, posy, sizex, sizey, color){
+    <div className={ styles.card }></div>
+}
---
+++ src/Game/Tile/TileFunctional.js
@@ -0,0 +1,4 @@
+
+function TileFunctional(posx, posy, piece){
+    
+}
